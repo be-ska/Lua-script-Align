6ddb87809424af97fc6f0a7af8069095df21cc8e
Luca Pescante <luca@pescante.it>
2023-09-23 17:20:40 +0800

    add control of pic, video and zoom

--- M460_camera-GPS.lua
+++ M460_camera-GPS.lua
@@ -1,7 +1,8 @@
--- camera-GPS.lua: Send GNSS coordinates to camera through custom serial protocol
+-- camera-GPS.lua: Send GNSS coordinates to camera through custom serial protocol and control camera
+-- Set CAM1_TYPE = 4 (Mount) to enable the camera driver
 
 -- user selectable parameters
-local DEBUG = 0
+local DEBUG = 2
 local BAUD_RATE = 115200
 local INIT_INTERVAL_MS = 3000 -- attempt to initialise the camera and GPS at this interval
 local UPDATE_INTERVAL_MS = 500 -- update at 2 Hz
@@ -9,6 +10,7 @@ local UPDATE_INTERVAL_MS = 500 -- update at 2 Hz
 -- global definitions
 local MAV_SEVERITY = {EMERGENCY = 0, ALERT = 1, CRITICAL = 2, ERROR = 3, WARNING = 4, NOTICE = 5, INFO = 6, DEBUG = 7}
 local GPS_INSTANCE = gps:primary_sensor()
+local MOUNT_INSTANCE = 0                -- always control the first mount/gimbal
 
 -- packet parsing definitions
 local HEADER_SEND = 0xAA -- header send
@@ -21,8 +23,14 @@ local PARSE_STATE_WAITING_FOR_LENGTH = 2
 local PARSE_STATE_WAITING_FOR_DATA = 3
 
 -- Camera Operation Bytes
-local CAM_SET_EXIF = 0x08
-local CAM_SET_EXIF_GPS = 0x01
+local CAM_CMD_EXIF = 0x08
+local CAM_PRM_EXIF_GPS = 0x01
+local CAM_CMD_SHORTCUT = 0x01
+local CAM_PRM_TAKE_PIC = 0x04
+local CAM_PRM_REC_VIDEO = 0x05
+local CAM_PRM_ZOOM = 0x08
+local CAM_PRM_ZOOM_IN = 0x02
+local CAM_PRM_ZOOM_OUT = 0x03
 
 -- local variables and definitions
 local uart  -- uart object connected to mount
@@ -32,6 +40,11 @@ local parse_length = 0 -- incoming message parsed length
 local parse_operation = 0 -- incoming message command id
 local parse_data_buff = {} -- parsed data buffer
 local parse_data_bytes_recv = 0 -- count of the number of bytes received in the message so far
+local cam_pic_count = 0                 -- last picture count.  used to detect trigger pic
+local cam_rec_video = false             -- last record video state.  used to detect record video
+local cam_zoom_step = 0                 -- last zoom step state.  zoom out = -1, hold = 0, zoom in = 1
+local cam_focus_step = 0                -- last focus step state.  focus in = -1, focus hold = 0, focus out = 1
+local cam_autofocus = false             -- last auto focus state
 
 -- parsing status reporting variables
 local last_ms = 0 -- system time that debug output was last printed
@@ -167,7 +180,7 @@ end
 -- send EXIF GPS message
 function send_GPS()
   -- check GPS fix
-  if gps:status(GPS_INSTANCE) < 3 and DEBUG < 1 then
+  if gps:status(GPS_INSTANCE) < 3 and DEBUG < 3 then
     -- wait for GPS fix
     return false
   end
@@ -187,7 +200,7 @@ function send_GPS()
   local velocity = math.floor(((velocity_vec:x() ^ 2 + velocity_vec:y() ^ 2) ^ 0.5) * 3600 + 0.5) -- km/h E3
 
   -- debug packet, override with default datas
-  if DEBUG > 1 then
+  if DEBUG > 2 then
     lng = 1139359819
     lat = 225467631
     alt_sign = "+"
@@ -198,7 +211,7 @@ function send_GPS()
     date = 20200511
   end
 
-  if DEBUG > 0 then
+  if DEBUG > 1 then
     gcs:send_text(MAV_SEVERITY.INFO, string.format("Long: %d, Lat: %d, Alt: %d, Vel: %d", lng, lat, alt, velocity))
   end
 
@@ -206,8 +219,8 @@ function send_GPS()
   local packet_to_send = {
     HEADER_SEND, -- header
     31, -- length (including header and parity bit)
-    CAM_SET_EXIF, -- cmd 1
-    CAM_SET_EXIF_GPS, -- cmd 2
+    CAM_CMD_EXIF, -- cmd 1
+    CAM_PRM_EXIF_GPS, -- cmd 2
     byte1(lng),
     byte2(lng),
     byte3(lng),
@@ -251,6 +264,52 @@ function read_incoming_packets()
   while n_bytes > 0 do
     n_bytes = n_bytes - 1
     parse_byte(uart:read())
+  end
+end
+
+-- check for changes in camera state and send messages if required
+function check_camera_state()
+
+  -- get latest state from AP driver
+  local pic_count, rec_video, zoom_step, focus_step, auto_focus = mount:get_camera_state(MOUNT_INSTANCE)
+
+  -- check for take picture
+  if pic_count and pic_count ~= cam_pic_count then
+    cam_pic_count = pic_count
+    local packet_to_send = {HEADER_SEND, 0x05, CAM_CMD_SHORTCUT, CAM_PRM_TAKE_PIC, 0x0A}
+    write_bytes(packet_to_send, #packet_to_send)
+    if DEBUG > 0 then
+      gcs:send_text(MAV_SEVERITY.INFO, string.format("CAM: took pic %u", pic_count))
+    end
+  end
+
+  -- check for start/stop recording video
+  if rec_video ~= cam_rec_video then
+    cam_rec_video = rec_video
+    local packet_to_send = {HEADER_SEND, 0x05, CAM_CMD_SHORTCUT, CAM_PRM_REC_VIDEO, 0xDF}
+    write_bytes(packet_to_send, #packet_to_send)
+    if DEBUG > 0 then
+      gcs:send_text(MAV_SEVERITY.INFO, "CAM: rec video:" .. tostring(cam_rec_video))
+    end
+  end
+    
+  -- check manual zoom
+  -- zoom out = -1, hold = 0, zoom in = 1
+  if zoom_step ~= cam_zoom_step then
+    cam_zoom_step = zoom_step
+    local packet_to_send = {HEADER_SEND, 0x06, CAM_CMD_SHORTCUT, CAM_PRM_ZOOM, 0, 0}
+    if cam_zoom_step > 0 then
+      packet_to_send[5] = CAM_PRM_ZOOM_IN
+      packet_to_send[6] = 0x11
+      write_bytes(packet_to_send, #packet_to_send)
+    elseif cam_zoom_step < 0 then
+      packet_to_send[5] = CAM_PRM_ZOOM_OUT
+      packet_to_send[6] = 0xC4
+      write_bytes(packet_to_send, #packet_to_send)
+    end
+    if DEBUG > 0 then
+      gcs:send_text(MAV_SEVERITY.INFO, "CAM: zoom:" .. tostring(cam_zoom_step))
+    end
   end
 end
 
@@ -300,7 +359,7 @@ function write_bytes(buff, len)
     packet_string = packet_string .. byte_to_write .. " "
   end
 
-  if DEBUG > 0 then
+  if DEBUG > 1 then
     gcs:send_text(MAV_SEVERITY.INFO, packet_string)
   end
 
@@ -317,6 +376,9 @@ function update()
 
   -- consume incoming bytes
   read_incoming_packets()
+
+  -- camera control
+  check_camera_state()
 
   -- send EXIF GPS
   if not send_GPS() then
