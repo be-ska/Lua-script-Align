f599ce41ed664dbfd5713e7ff75419f2b6d805f3
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2023-12-13 09:44:55 +0800

    publish level with send_named_float at 1 Hz

--- agricolture_level.lua
+++ agricolture_level.lua
@@ -1,23 +1,10 @@
 -- control nozzle based on pump, check sprayer level
 
 -- user parameters
-local MILLIS_UPDATE = 100
+local MILLIS_UPDATE = 1000
 local LEVEL_PIN = 59 -- AP3 ch 10
 
--- parameters
-local PARAM_TABLE_KEY = 42
-assert(param:add_table(PARAM_TABLE_KEY, "AGR_", 2), "could not add param table")
-assert(param:add_param(PARAM_TABLE_KEY, 1, "DEBUG", 0), "could not add param 1")
-assert(param:add_param(PARAM_TABLE_KEY, 2, "LEVEL", 0), "could not add param 2")
-
--- bind parameters to variables
-local AGR_DEBUG = Parameter()
-local AGR_LEVEL = Parameter()
-AGR_DEBUG:init("AGR_DEBUG")
-AGR_LEVEL:init("AGR_LEVEL")
-
 -- global variables and init
-local _level_state = -1
 gpio:pinMode(LEVEL_PIN,0) -- set level pin as input
 
 function check_level()
@@ -27,21 +14,10 @@ function check_level()
     if gpio:read(LEVEL_PIN) then
         level_state_now = 1
     end
-    -- compare with level state saved, if different update parameter
-    if level_state_now ~= _level_state then
-        if level_state_now == 1 then
-            AGR_LEVEL:set_and_save(1)
-            _level_state = 1
-            if AGR_DEBUG:get() > 0 then
-                gcs:send_text('6', "Level state 1")
-            end
-        else
-            AGR_LEVEL:set_and_save(0)
-            _level_state = 0
-            if AGR_DEBUG:get() > 0 then
-                gcs:send_text('6', "Level state 0")
-            end
-        end
+    if level_state_now == 1 then
+        gcs:send_named_float("level", 1)
+    else
+        gcs:send_named_float("level", 0)
     end
 
     return check_level, MILLIS_UPDATE
