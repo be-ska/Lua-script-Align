dc3ed2133f57f590736f38762e687601b3dea7ee
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-02-11 16:51:12 +0100

    add starter timeout

--- GA22_engine.lua
+++ GA22_engine.lua
@@ -27,15 +27,25 @@ ENG_REV_IGN:init("ENG_REV_IGN")
 ENG_REV_STR:init("ENG_REV_STR")
 ENG_CH:init("ENG_CH")
 
--- global variables and init
+-- costants
 local MODE_HOLD = 4
+local STARTER_TIMEOUT_MS = 10000 -- starter timeout to prevent starter failure
+local STARTER_OFF = 0
+local STARTER_ON = 1
+local STARTER_ON_TIMEOUT = 2
+local STARTER_INIT = -1
+
+--global variables
 local _eng_ch = 0
 local _fuel_state = -1
-local _starter_first = true
 local ignition_on = 1
 local ignition_off = 0
 local start_on = 1
 local start_off = 0
+local starter_state = STARTER_INIT
+local starter_on_ms = uint32_t(0)
+
+-- init gpio
 gpio:pinMode(FUEL_PIN,0) -- set fuel pin as input
 gpio:pinMode(IGNITION_PIN,1) -- set ignition pin as output
 gpio:write(IGNITION_PIN, ignition_off) -- set ignition off
@@ -53,6 +63,7 @@ function check_channels_params()
 end
 
 function set_ignition()
+    -- turn off engine if in hold mode (failsafe)
     if vehicle:get_mode() == MODE_HOLD then
         gpio:write(IGNITION_PIN, ignition_off)
         if ENG_DEBUG:get() > 1 then
@@ -72,20 +83,40 @@ function set_ignition()
 end
 
 function set_starter()
-    if _starter_first then
+    if starter_state == STARTER_INIT then
         gpio:write(START_PIN, start_off)
         if rc:get_pwm(_eng_ch) < 1800 then
-            _starter_first = false
+            starter_state = STARTER_OFF
         end
-    elseif rc:get_pwm(_eng_ch) > 1850 then
-        gpio:write(START_PIN, start_on)
-        if ENG_DEBUG:get() > 1 then
-            gcs:send_text('6', "Starter ON")
+    elseif starter_state == STARTER_OFF then
+        if rc:get_pwm(_eng_ch) > 1850 then
+            starter_on_ms = millis()
+            gpio:write(START_PIN, start_on)
+            starter_state = STARTER_ON
+            if ENG_DEBUG:get() > 1 then
+                gcs:send_text('6', "Starter ON")
+            end
         end
-    else
-        gpio:write(START_PIN, start_off)
-        if ENG_DEBUG:get() > 1 then
-            gcs:send_text('6', "Starter OFF")
+    elseif starter_state == STARTER_ON then
+        if rc:get_pwm(_eng_ch) < 1800 then
+            gpio:write(START_PIN, start_off)
+            starter_state = STARTER_OFF
+            if ENG_DEBUG:get() > 1 then
+                gcs:send_text('6', "Starter OFF")
+            end
+        elseif millis() - starter_on_ms > STARTER_TIMEOUT_MS then
+            gpio:write(START_PIN, start_off)
+            starter_state = STARTER_ON_TIMEOUT
+            if ENG_DEBUG:get() > 1 then
+                gcs:send_text('6', "Starter TIMEOUT")
+            end
+        end
+    elseif starter_state == STARTER_ON_TIMEOUT then
+        if rc:get_pwm(_eng_ch) < 1800 then
+            starter_state = STARTER_OFF
+            if ENG_DEBUG:get() > 1 then
+                gcs:send_text('6', "Starter OFF")
+            end
         end
     end
 end
