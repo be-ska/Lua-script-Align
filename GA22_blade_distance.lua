4324f4e66e2ab7e55f879f6137260e91528aef7e
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-07-08 10:20:28 +0200

    add realy capability for controlling blade distance instead of PWM

--- GA22_blade_distance.lua
+++ GA22_blade_distance.lua
@@ -1,6 +1,8 @@
 -- control the blade distance from the ground of Align mowers - version 1.2
 
 -- user parameters
+local RELAY_PIN_UP = 00 -- TODO
+local RELAY_PIN_DOWN = 00 -- TODO
 local MILLIS_FULL_UP = 12000
 local STEP_MAX = 6
 local PWM_UP = 2000
@@ -10,13 +12,14 @@ local MILLIS_UPDATE = 100
 
 -- parameters
 local PARAM_TABLE_KEY = 40
-assert(param:add_table(PARAM_TABLE_KEY, "BLADE_", 6), "could not add param table")
+assert(param:add_table(PARAM_TABLE_KEY, "BLADE_", 7), "could not add param table")
 assert(param:add_param(PARAM_TABLE_KEY, 1, "DEBUG", 0), "could not add param 1")
 assert(param:add_param(PARAM_TABLE_KEY, 2, "CURR", 0), "could not add param 2")
 assert(param:add_param(PARAM_TABLE_KEY, 3, "SET", 0), "could not add param 3")
 assert(param:add_param(PARAM_TABLE_KEY, 4, "STEP", 2000), "could not add param 4")
 assert(param:add_param(PARAM_TABLE_KEY, 5, "REV", 0), "could not add param 5")
 assert(param:add_param(PARAM_TABLE_KEY, 6, "CH", 2), "could not add param 6")
+assert(param:add_param(PARAM_TABLE_KEY, 7, "RELAY", 1), "could not add param 7")
 
 -- bind parameters to variables
 local BLADE_DEBUG = Parameter()
@@ -25,21 +28,40 @@ local BLADE_SET_HEIGHT = Parameter()
 local BLADE_STEP = Parameter()
 local BLADE_REVERSE = Parameter()
 local BLADE_CH = Parameter()
+local BLADE_RELAY = Parameter()
 BLADE_DEBUG:init("BLADE_DEBUG")
 BLADE_CURRENT_HEIGHT:init("BLADE_CURR")
 BLADE_SET_HEIGHT:init("BLADE_SET")
 BLADE_STEP:init("BLADE_STEP")
 BLADE_REVERSE:init("BLADE_REV")
 BLADE_CH:init("BLADE_CH")
-local STEP_TO_MILLIS = BLADE_STEP:get()
+BLADE_RELAY:init("BLADE_RELAY")
+local USE_RELAY = BLADE_RELAY:get()
 
 -- global variables
 local pwm_up = PWM_UP
 local pwm_down = PWM_DOWN
 local channel = 1
 
+function set_output(pwm)
+    if USE_RELAY > 0 then
+        if pwm > PWM_NEUTRAL then
+            gpio:write(RELAY_PIN_DOWN, 0)
+            gpio:write(RELAY_PIN_UP, 1)
+        elseif pwm < PWM_NEUTRAL then
+            gpio:write(RELAY_PIN_UP, 0)
+            gpio:write(RELAY_PIN_DOWN, 1)
+        else
+            gpio:write(RELAY_PIN_UP, 0)
+            gpio:write(RELAY_PIN_DOWN, 0)
+        end
+    else
+        SRV_Channels:set_output_pwm_chan(channel, pwm)
+    end
+end
+
 function update()
-    SRV_Channels:set_output_pwm_chan(channel, PWM_NEUTRAL)
+    set_output(PWM_NEUTRAL)
     -- get params
     STEP_TO_MILLIS = BLADE_STEP:get()
     local current_height = BLADE_CURRENT_HEIGHT:get()
@@ -73,7 +95,7 @@ function update()
         end
         BLADE_CURRENT_HEIGHT:set_and_save(STEP_MAX)
         BLADE_SET_HEIGHT:set_and_save(STEP_MAX)
-        SRV_Channels:set_output_pwm_chan_timeout(channel, pwm_up, MILLIS_FULL_UP)
+        set_output(pwm_up)
         return update, MILLIS_FULL_UP
     elseif set_height > current_height then
         -- move blade up
@@ -86,7 +108,7 @@ function update()
             -- when going full up leave the servo for 500 ms longer, just to be sure to reach the maximum
             timeout = timeout + 500
         end
-        SRV_Channels:set_output_pwm_chan_timeout(channel, pwm_up, timeout)
+        set_output(pwm_up)
         return update, timeout
     elseif set_height < current_height then
         -- move blade down
@@ -99,26 +121,35 @@ function update()
             -- when going full down leave the servo for 500 ms longer, just to be sure to reach the minimum
             timeout = timeout + 500
         end
-        SRV_Channels:set_output_pwm_chan_timeout(channel, pwm_down, timeout)
+        set_output(pwm_down)
         return update, timeout
     end
     return update, MILLIS_UPDATE
 end
 
 gcs:send_text('6', "blade_distance.lua is running")
-SRV_Channels:set_output_pwm_chan(channel, PWM_NEUTRAL)
+
+-- chek use relay
+if USE_RELAY > 0 then
+    gpio:pinMode(RELAY_PIN_UP,1) -- set RELAY pin as output
+    gpio:pinMode(RELAY_PIN_DOWN,1) -- set RELAY pin as output
+else
+    -- use PWM output: check channel
+    if BLADE_CH:get() == nil or BLADE_CH:get() < 1 then
+        channel = 1
+    else
+    -- on old AP 4.2.x RC channels index is [0 15], newer firmwares is [1 16]
+        channel = BLADE_CH:get() - 1
+    end
+end
+
+-- be sure to start in neutral position
+set_output(PWM_NEUTRAL)
 
 -- check reverse parameter
 if BLADE_REVERSE:get() > 0 then
     pwm_up = PWM_DOWN
     pwm_down = PWM_UP
-end
-
-if BLADE_CH:get() == nil or BLADE_CH:get() < 1 then
-    channel = 1
-else
--- on old AP 4.2.x RC channels index is [0 15], newer firmwares is [1 16]
-    channel = BLADE_CH:get() - 1
 end
 
 return update, 1000
