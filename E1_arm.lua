58c7b085c04356ba2a1c4a3418ba578120776ef3
Luca Pescante <luca@pescante.it>
2025-04-10 16:49:28 +0800

    E1_arm: add vibration check

--- E1_arm.lua
+++ E1_arm.lua
@@ -1,4 +1,4 @@
--- custom arming routine for Align helicopters - version 1.0
+-- custom arming routine for Align helicopters - version 1.1
 
 -- constants
 local AUX_FUNCTION_MOTOR_INTERLOCK = 32
@@ -10,11 +10,20 @@ local STATES = {    WAIT_INTERLOCK_LOW = 0,
                     WAIT_THROTTLE_LOW_TIMEOUT = 3,
                     ARM = 4,
                     ARMED_ON = 5,
-                    ARMED_OFF = 6 }
+                    ARMED_OFF = 6,
+                    ARMED_ON_SPOOLUP = 7,
+                    ERROR_VIBRATIONS = 8
+                }
 
 -- global variables
 local state = STATES.WAIT_INTERLOCK_LOW
 local time_ms = uint32_t(0)
+
+-- parameters
+local PARAM_TABLE_KEY = 46
+assert(param:add_table(PARAM_TABLE_KEY, "VIBE_", 1), "could not add param table")
+assert(param:add_param(PARAM_TABLE_KEY, 1, "TKOFF_MAX", 60), "could not add G3P_DEBUG param")
+local VIBE_TKOFF_MAX = Parameter("VIBE_TKOFF_MAX")
 
 function update()
     if state == STATES.WAIT_INTERLOCK_LOW then
@@ -60,15 +69,35 @@ function update()
             rc:run_aux_function(AUX_FUNCTION_MOTOR_INTERLOCK, AUX_LOW)
             state = STATES.ARM
         end
- 
+
     elseif state == STATES.ARM then
         if arming:arm() then
             -- motor interlock ON
             rc:run_aux_function(AUX_FUNCTION_MOTOR_INTERLOCK, AUX_HIGH)
-            state = STATES.ARMED_ON
+            state = STATES.ARMED_ON_SPOOLUP
         else
             -- arm fail, go back to wait interlock low
             state = STATES.WAIT_INTERLOCK_LOW
+        end
+
+    elseif state == STATES.ARMED_ON_SPOOLUP then
+        if not arming:is_armed() then
+            state = STATES.WAIT_INTERLOCK_LOW
+        end
+        if rc:get_aux_cached(AUX_FUNCTION_MOTOR_INTERLOCK) == AUX_LOW then
+            -- user turned off motor
+            time_ms = millis()
+            state = STATES.ARMED_OFF
+        end
+        -- wait for spoolup
+        if motors:get_spool_state() == 3 then
+            state = STATES.ARMED_ON
+        end
+        -- check vibrations
+        if ahrs:get_vibration():x() > VIBE_TKOFF_MAX:get() or ahrs:get_vibration():y() > VIBE_TKOFF_MAX:get() or ahrs:get_vibration():z() > VIBE_TKOFF_MAX:get() then
+            -- turn off motor
+            rc:run_aux_function(AUX_FUNCTION_MOTOR_INTERLOCK, AUX_LOW)
+            state = STATES.ERROR_VIBRATIONS
         end
 
     elseif state == STATES.ARMED_ON then
@@ -89,14 +118,21 @@ function update()
             -- user turned on motor
             state = STATES.ARMED_ON
         end
-        if millis() - time_ms > 10000 then
-            -- try disarm after 10 seconds if vehicle is not flying
-            -- if disarm fails wait 10 more seconds before trying again
-            if not vehicle:get_likely_flying() then
-                time_ms = millis()
-                if arming:disarm() then
-                    state = STATES.WAIT_INTERLOCK_LOW
-                end
+        -- wait for spooldown and disarm
+        if motors:get_spool_state() < 2 then
+            if arming:disarm() then
+                state = STATES.WAIT_INTERLOCK_LOW
+            end
+        end
+
+    elseif state == STATES.ERROR_VIBRATIONS then
+        -- no way to get out this state, to prevent user flying again
+        if arming:is_armed() then
+            -- force motor off
+            rc:run_aux_function(AUX_FUNCTION_MOTOR_INTERLOCK, AUX_LOW)
+            -- wait for spooldown and disarm
+            if motors:get_spool_state() < 2 then
+                arming:disarm()
             end
         end
     end
