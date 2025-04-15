213d2b4718e6710254ca10bc727d18f41e801a8c
Luca Pescante <luca@pescante.it>
2025-04-15 13:01:37 +0800

    E1_arm: add fan controller

--- E1_arm.lua
+++ E1_arm.lua
@@ -1,6 +1,8 @@
--- custom arming routine for Align helicopters - version 1.1
+-- custom arming routine for Align helicopters - version 1.2
 
 -- constants
+local FAN_RELAY = 1
+local BATT_INSTANCE = 0
 local AUX_FUNCTION_MOTOR_INTERLOCK = 32
 local AUX_HIGH = 2
 local AUX_LOW = 0
@@ -18,6 +20,7 @@ local STATES = {    WAIT_INTERLOCK_LOW = 0,
 -- global variables
 local state = STATES.WAIT_INTERLOCK_LOW
 local time_ms = uint32_t(0)
+local count = 0
 
 -- parameters
 local PARAM_TABLE_KEY = 46
@@ -25,7 +28,44 @@ assert(param:add_table(PARAM_TABLE_KEY, "VIBE_", 1), "could not add param table"
 assert(param:add_param(PARAM_TABLE_KEY, 1, "TKOFF_MAX", 60), "could not add G3P_DEBUG param")
 local VIBE_TKOFF_MAX = Parameter("VIBE_TKOFF_MAX")
 
-function update()
+function fan_control()
+    -- fan always on when armed
+    if state == STATES.ARMED_ON then
+        if relay:get(FAN_RELAY) == 0 then
+            relay:on(FAN_RELAY)
+        end
+        return
+    end
+
+    -- turn on fan if battery temperature is high
+    local batt_temp_fan_on = param:get("BATT_SERIAL_NUM")
+    local batt_temp = battery:get_temperature(BATT_INSTANCE)
+
+    -- check nil or out of range variables
+    if batt_temp_fan_on == nil then
+        batt_temp_fan_on = 50
+    end
+    if batt_temp == nil then
+        -- fan always ON if no motor temperature available
+        if relay:get(FAN_RELAY) == 0 then
+            relay:on(FAN_RELAY)
+        end
+        return
+    end
+
+    -- fan logic
+    if batt_temp > batt_temp_fan_on then
+        if relay:get(FAN_RELAY) == 0 then
+            relay:on(FAN_RELAY)
+        end
+    elseif batt_temp < batt_temp_fan_on - 5 then
+        if relay:get(FAN_RELAY) == 1 then
+            relay:off(FAN_RELAY)
+        end
+    end
+end
+
+function arm_control()
     if state == STATES.WAIT_INTERLOCK_LOW then
         if rc:get_pwm(3) < 950 then
             -- rc not ready
@@ -37,8 +77,8 @@ function update()
         end
 
     elseif state == STATES.WAIT_INTERLOCK_HIGH then
-        -- need motor interlock high before arming routine
-        if rc:get_aux_cached(AUX_FUNCTION_MOTOR_INTERLOCK) == AUX_HIGH then
+        -- need motor interlock high before arming routine and throttle not low
+        if rc:get_aux_cached(AUX_FUNCTION_MOTOR_INTERLOCK) == AUX_HIGH and rc:get_pwm(3) > 1150 then
             time_ms = millis()
             state = STATES.WAIT_THROTTLE_LOW
         end
@@ -136,9 +176,21 @@ function update()
             end
         end
     end
+
+    -- send arming state to GCS
     gcs:send_named_float("arm", state)
+end
+
+function update()
+    -- call arm_control at 10 Hz and fan_control at 1 Hz
+    count = count + 1
+    arm_control()
+    if count > 9 then
+        count = 0
+        fan_control()
+    end
     return update, 100
 end
 
 gcs:send_text('6', "E1_arm.lua is running")
-return update, 100
+return update, 2000
